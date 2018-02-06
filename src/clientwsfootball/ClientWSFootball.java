367027cb289845fab5fd21cd4c13c513ec83c404
entrar <entrar@PC-1201>
2018-02-06 13:44:18 +0000

    Ingeniería de Sistemas, ULPGC

---
+++ src/clientwsfootball/ClientWSFootball.java
@@ -0,0 +1,48 @@
+package clientwsfootball;
+
+import eu.dataaccess.footballpool.ArrayOfString;
+import eu.dataaccess.footballpool.ArrayOftCountryInfo;
+
+public class ClientWSFootball {
+
+ 
+    public static void main(String[] args) {
+        //System.out.print("Yellow cards total = " + yellowCardsTotal());
+        //System.out.print("Todos los defensas = " + allDefenders("Lyon").getString());
+        
+        // Todos los defensas de todas las ciudades
+        String result = "";
+        
+        for(Object city : cities().getString()) {
+            result += city.toString() + ":\n" + allDefenders(city.toString()).getString() + "\n";
+        }
+        
+        System.out.print(result);
+        
+    }
+
+    private static int yellowCardsTotal() {
+        eu.dataaccess.footballpool.Info service = new eu.dataaccess.footballpool.Info();
+        eu.dataaccess.footballpool.InfoSoapType port = service.getInfoSoap();
+        return port.yellowCardsTotal();
+    }
+
+    private static ArrayOfString allDefenders(java.lang.String sCountryName) {
+        eu.dataaccess.footballpool.Info service = new eu.dataaccess.footballpool.Info();
+        eu.dataaccess.footballpool.InfoSoapType port = service.getInfoSoap();
+        return port.allDefenders(sCountryName);
+    }
+
+    private static ArrayOfString cities() {
+        eu.dataaccess.footballpool.Info service = new eu.dataaccess.footballpool.Info();
+        eu.dataaccess.footballpool.InfoSoapType port = service.getInfoSoap();
+        return port.cities();
+    }
+
+    private static ArrayOftCountryInfo countryNames(boolean bWithCompetitors) {
+        eu.dataaccess.footballpool.Info service = new eu.dataaccess.footballpool.Info();
+        eu.dataaccess.footballpool.InfoSoapType port = service.getInfoSoap();
+        return port.countryNames(bWithCompetitors);
+    }
+    
+}
