159c8310295ba27fa1dbf40fe267ee886c702c92
QueraltSosaMompel <queraltsosa@hotmail.com>
2018-02-24 23:39:42 +0000

    Final version

--- src/clientwsfootball/ClientWSFootball.java
+++ src/clientwsfootball/ClientWSFootball.java
@@ -4,32 +4,69 @@ import eu.dataaccess.footballpool.ArrayOfString;
 import eu.dataaccess.footballpool.ArrayOftCountryInfo;
 import eu.dataaccess.footballpool.TCountryInfo;
 
+import java.util.Scanner;
+
 public class ClientWSFootball {
 
  
     public static void main(String[] args) {
-        //System.out.print("Yellow cards total = " + yellowCardsTotal());
-        //System.out.print("Todos los defensas = " + allDefenders("Lyon").getString());
+        System.out.print("1 - Get yellow cards\n\n2 - Get all defenders"
+                + "\n\n3 - Get all defenders of all countries.\n\nEnter a "
+                + "option: ");
         
-        // Todos los defensas de todos los países 
-        String result = "";
-        for(TCountryInfo info : countryNames(true).getTCountryInfo()) {
-                result += info.getSName() + "\n" + allDefenders(info.getSName()).getString() + "\n";     
+        int selection = new Scanner(System.in).nextInt();
+        
+        switch (selection) {
+            case 1: 
+                System.out.print(getYellowCards());
+                break;
+                
+            case 2:
+                System.out.print(getAllDefenders());
+                break;
+                
+            case 3: 
+                System.out.print(getDefendersAllCountries());
+                break;
+               
+            default: 
+                System.out.println("\nThe number of selection is incorrect.");
+                
         }
-        
-        System.out.print(result);
-        
-        // Todos los defensas de todas las ciudades
-        result = "";
-        
-        for(Object city : cities().getString()) {
-            result += city.toString() + ":\n" + allDefenders(city.toString()).getString() + "\n";
-        }
-        
-        //System.out.print(result);
+       
         
     }
 
+    /*
+    * @return : All yellow cards in total
+    */
+    private static String getYellowCards() {
+        return ("Yellow cards total = " + yellowCardsTotal());
+    }
+    
+    /*
+    * @return : All all defenders of one city that user entered 
+    */
+    private static String getAllDefenders() {
+        System.out.print("Enter a city/country: ");
+        String city = new Scanner(System.in).nextLine();
+        return("All defenders of " + city + " are: \n" + 
+                allDefenders(city).getString());
+    }
+    
+    /*
+    * @return : All all defenders of all the countries
+    */
+    private static String getDefendersAllCountries() {
+        String result = "";
+                for(TCountryInfo info : countryNames(true).getTCountryInfo()) {
+                    result += info.getSName() + "\n" + 
+                            allDefenders(info.getSName()).getString() + "\n";     
+                }
+        return result;
+    }
+    
+    
     private static int yellowCardsTotal() {
         eu.dataaccess.footballpool.Info service = new eu.dataaccess.footballpool.Info();
         eu.dataaccess.footballpool.InfoSoapType port = service.getInfoSoap();
